a5ce497d22dee25cd4d785213816edf610965dd5
an1age <vladimirtyo7@gmail.com>
2021-11-17 17:54:14 +0600

    Сумматор 1.0

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            string userAnswer = "";
+
+            do
+            {
+                try
+                {
+                    Console.Write("Введите число: ");
+                    userAnswer = Console.ReadLine();
+
+                    result += Convert.ToInt32(userAnswer);
+                    Console.WriteLine($"Текущая сумма: {result}\n");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + "\n");
+                }
+            }
+            while (userAnswer != "exit");
+
+        }
+    }
+}
